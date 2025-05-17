d0c40363c96fe9dfa1fa5a9be21c15b6e2ad509a
LaneLR <laneleerichardson@gmail.com>
2025-05-17 18:23:58 -0500

    add user icon and menu selector to navbar, adjusted footer padding and size

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <header>
-        <div className='left-nav'></div>
-        <div className='center-nav'><img alt="The Bible Brief Logo" src='/bible-brief-app/biblebrief-logo.jpg'/></div>
-        <div className='right-nav'></div>
+        <div className="left-nav">
+          <p>|||</p>
+        </div>
+        <div className="center-nav">
+          <img
+            alt="The Bible Brief Logo"
+            src="/bible-brief-app/biblebrief-logo.jpg"
+          />
+        </div>
+        <div className="right-nav">
+            <img
+              alt="The Bible Brief Logo"
+              src="/bible-brief-app/user-icon.png"
+            />
+        </div>
       </header>
-      <section>
-        <div className='navbar'></div>
-      </section>
+      <main>
+        <section className="navbar"></section>
+        <section className="card-wrapper">
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+        </section>
+      </main>
       <footer>
         <p>TheBibleBrief.org</p>
         <p>Copyright © 2025 The Bible Brief - All Rights Reserved.</p>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
