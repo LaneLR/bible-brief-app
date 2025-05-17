bf8bca812b0f00c0413414e7d40908cd9bae7615
LaneLR <laneleerichardson@gmail.com>
2025-05-17 13:54:57 -0500

    moved images to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <header>
+        <div className='left-nav'></div>
+        <div className='center-nav'><img alt="The Bible Brief Logo" src='../public/biblebrief-logo.jpg'/></div>
+        <div className='right-nav'></div>
+      </header>
       <section>
         <div className='navbar'></div>
       </section>
+      <footer>
+        <p>TheBibleBrief.org</p>
+        <p>Copyright © 2025 The Bible Brief - All Rights Reserved.</p>
+      </footer>
     </>
   )
 }
