dd2bd51deab692ad46f855118e58817da0732d0b
LaneLR <laneleerichardson@gmail.com>
2025-05-20 20:49:38 -0500

    added border-box to AppWrapper

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Main from "./components/Body";
 import { ScreenOrientation } from "@awesome-cordova-plugins/screen-orientation";
 
 const AppWrapper = styled.div`
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
   height: 100vh;
