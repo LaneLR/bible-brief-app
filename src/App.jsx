0bb8e999670cca67572200bd5613be9b62460d89
LaneLR <laneleerichardson@gmail.com>
2025-05-26 18:05:48 -0500

    add routes for app navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,37 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Newsletter from "./pages/Newsletter";
 import About from "./pages/About";
+import Group from "./pages/Group";
 import styled from "styled-components";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Body";
-import { ScreenOrientation } from "@awesome-cordova-plugins/screen-orientation";
+import Navbar from "./components/Navbar";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import Body from "./components/Body";
 
 const AppWrapper = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
   height: 100vh;
+  margin: 0;
+  padding: 0;
 `;
 
 function App() {
-  document.addEventListener("deviceready", () => {
-    ScreenOrientation.lock(ScreenOrientation.ORIENTATIONS.PORTRAIT);
-  });
-
   return (
     <AppWrapper>
       <Header />
-      <Main />
-      <Footer />
+      <Body>
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/newsletter" element={<Newsletter />} />
+          <Route path="/group" element={<Group />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Body>
+      <Navbar />
     </AppWrapper>
   );
 }
